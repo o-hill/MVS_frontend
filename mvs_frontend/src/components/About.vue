31476035d513d9261035e5fcbac75edde1d7b677
Oliver Hill <ohill@MATTHEWs-MacBook-Pro-2.local>
2019-01-28 08:22:03 -0500

    Cleaning up and starting about.

--- mvs_frontend/src/components/About.vue
+++ mvs_frontend/src/components/About.vue
@@ -1,1 +1,47 @@
-vue
+<template>
+
+</template>
+
+
+<script>
+
+  // import Component from '../componentLocation'
+
+  export default {
+
+  name: 'Default',
+
+  	components: { },
+
+  	props: [ ],
+
+  	data() {
+  	  return {
+  	  	// data!
+  	  }
+  	},
+
+  	watch: {
+
+  	},
+
+  	methods: {
+
+  	},
+
+  	computed: {
+
+  	},
+
+  	mounted() {
+
+  	}
+
+  }
+
+</script>
+
+
+<style>
+
+</style>
